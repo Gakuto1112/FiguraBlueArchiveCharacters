94fff59199fe01cca9b5cd80ee717bcc6e18cbb9
Gakuto1112 <gakuto131112@gmail.com>
2023-09-06 23:22:44 +0900

    設置物の落下機能の実装

--- scripts/placement_object.lua
+++ scripts/placement_object.lua
@@ -2,11 +2,13 @@
 ---@field PLACEMENT_OBJECTS ModelPart 設置物として扱うモデル。指定したモデルをここでコピーして設置物とする。
 ---@field HITBOXES table<table<Vector3>> 設置物の当たり判定。直方体（立方体）のxyz座標がそれぞれ最小の頂点を指定し、そこからのxyz軸での長さを指定する。
 ---@field COLLISION_FINESS integer 当たり判定の細かさ（1ブロックの長さにつき何回処理を行うか）。当然細かくすれば精度が向上するが、その分処理の負荷も増大する。
+---@field FALL_SPEED number 空中にある設置物が落下する速度（ブロック/ティック）
 PlacementObject = {
     --定数
     PLACEMENT_OBJECTS = models.models.placement_object.PlacementObject,
     HITBOXES = {{vectors.vec3(-5, 0, -10), vectors.vec3(20, 38, 20)}},
-    COLLISION_FINESS = 1,
+    COLLISION_FINESS = 16,
+    FALL_SPEED = 1,
 
     --関数
     ---指定した場所に指定した向きで設置物を置く。
@@ -56,7 +58,7 @@ PlacementObject = {
                 if collisionDetected then
                     break
                 else
-                    for _, pos in ipairs(CollisionUtils:getCollisionBlocks(modelPart:getPos():scale(1 / 16):add(hitBox[1]), hitBox[2])) do
+                    for _, pos in ipairs(CollisionUtils:getCollisionBlocks(modelPos:copy():add(hitBox[1]), hitBox[2])) do
                         if collisionDetected then
                             break
                         else
@@ -81,6 +83,39 @@ PlacementObject = {
                         end
                     end
                 end
+            end
+            --落下先の判定
+            local fallDistance = 0
+            collisionDetected = false
+            for i = 0, -PlacementObject.FALL_SPEED, -(1 / PlacementObject.COLLISION_FINESS) do
+                for _, hitBox in ipairs(PlacementObject.HITBOXES) do
+                    for _, pos in ipairs(CollisionUtils:getCollisionBlocks(modelPos:copy():add(hitBox[1]):add(0, i), hitBox[2]:copy():mul(1, 0, 1))) do
+                        local block = world.getBlockState(pos)
+                        if block:hasCollision() then
+                            for _, collision in ipairs(block:getCollisionShape()) do
+                                local hitBoxStartPos = modelPos:copy():add(hitBox[1])
+                                if CollisionUtils:isCubeOverrapped(hitBoxStartPos, hitBoxStartPos:copy():add(hitBox[2]:copy():mul(1, 0, 1)), pos:copy():add(collision[1]), pos:copy():add(collision[2])) then
+                                    collisionDetected = true
+                                    break
+                                end
+                            end
+                        end
+                        if collisionDetected then
+                            break
+                        end
+                    end
+                    if collisionDetected then
+                        break
+                    else
+                        fallDistance = -i
+                    end
+                end
+                if collisionDetected then
+                    break
+                end
+            end
+            if fallDistance > 0 then
+                modelPart:setPos(modelPart:getPos():add(0, -fallDistance * 16))
             end
         end
     end
