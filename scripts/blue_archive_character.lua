e0c4010b5521be589d321b906e373248b35231a8
Gakuto1112 <gakuto131112@gmail.com>
2023-12-09 15:23:42 +0900

    基本情報を入力

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -28,44 +28,44 @@ BlueArchiveCharacter = {
         firstName = {
             ---英語
             ---@type string
-            en_us = "FirstName",
+            en_us = "Marie",
 
             ---日本語
             ---@type string
-            ja_jp = "名前"
+            ja_jp = "マリー"
         },
 
         ---生徒の苗字
         lastName = {
             ---英語
             ---@type string
-            en_us = "LastName",
+            en_us = "Iochi",
 
             ---日本語
             ---@type string
-            ja_jp = "苗字"
+            ja_jp = "伊落"
         },
 
         ---生徒所属の部活名
         clubName = {
             ---英語
             ---@type string
-            en_us = "ClubName",
+            en_us = "Sisterhood",
 
             ---日本語
             ---@type string
-            ja_jp = "部活名",
+            ja_jp = "シスターフッド",
         },
 
         ---生徒の誕生日
         birth = {
             ---誕生月
             ---@type integer
-            month = 1,
+            month = 9,
 
             ---誕生日
             ---@type integer
-            day = 1
+            day = 12
         }
     },
 
