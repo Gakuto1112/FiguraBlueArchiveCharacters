d94849604c049a679ca2a811313e06818444c9de
Gakuto1112 <gakuto131112@gmail.com>
2024-02-02 23:06:48 +0900

    パーティクルのマネージャークラスを作成

--- scripts/ex_skill/frame_particle.lua
+++ scripts/ex_skill/frame_particle.lua
@@ -3,6 +3,7 @@
 ---| "NORMAL" 通常のパーティクル
 ---| "FIGURA" Figuraマークのパーティクル（穴空き三角形）
 
+---@class FrameParticle Exスキルのフレームで使用するパーティクルの単一を管理するクラス
 FrameParticle = {
     ---フレームパーティクルのインスタンスを新しく生成する。
     ---@param particleId integer スポーンさせるパーティのID。一意の値にする。
@@ -20,15 +21,23 @@ FrameParticle = {
         ---@type number
         instance.counter = 0
 
+        ---レンダーイベントで実行する関数
+        ---@return boolean canDiscardInstance このインスタンスを破棄してもよいか
+        instance.render = function (self)
+            models.script_ex_skill_frame_particles["Particle_"..particleId]:scale(vectors.vec3(1, 1, 1):scale(1 - self.counter))
+            instance.counter = math.min(self.counter + 4 / client:getFPS(), 1)
+            return instance.counter == 1
+        end
+
         ---パーティクルを削除する。パーティクルを削除するとパーティクルの再生成ができないのでこのインスタンスは破棄する。
         instance.remove = function (self)
-            models.models.ex_skill_frame.Gui.ParticleArea:removeChild(models.models.ex_skill_frame.Gui.ParticleArea["Particle_"..self.id])
+            models.script_ex_skill_frame_particles:removeChild(models.script_ex_skill_frame_particles["Particle_"..self.id])
         end
 
         ---@diagnostic disable-next-line: redundant-parameter
-        models.models.ex_skill_frame.Gui.ParticleArea:addChild(models.models.ex_skill_frame.Particles["Particle"..(type == "NORMAL" and 1 or 2)]:copy("Particle_"..particleId))
-        models.models.ex_skill_frame.Gui.ParticleArea["Particle_"..particleId]:setPos(screenPos:copy():augmented(1):scale(-1))
-        models.models.ex_skill_frame.Gui.ParticleArea["Particle_"..particleId]:setRot(90, math.map(math.random(), 0, 1, 0, 360), 180)
+        models.script_ex_skill_frame_particles:addChild(models.models.ex_skill_frame.Particles["Particle"..(type == "NORMAL" and 1 or 2)]:copy("Particle_"..particleId))
+        models.script_ex_skill_frame_particles["Particle_"..particleId]:setPos(screenPos:copy():augmented(1):scale(-1))
+        models.script_ex_skill_frame_particles["Particle_"..particleId]:setRot(90, math.map(math.random(), 0, 1, 0, 360), 180)
 
         return instance
     end
---
+++ scripts/ex_skill/frame_particle_manager.lua
@@ -0,0 +1,62 @@
+---@class FrameParticleManager Exスキルのフレームで使用するパーティクルを管理するクラス
+FrameParticleManager = {
+    ---フレームパーティクルのインスタンスを保持するテーブル
+    ---@type table<table>
+    Particles = {},
+
+    ---フレームパーティクルのIDを管理するカウンター
+    ---@type integer
+    IdCounter = 1,
+
+    ---レンダーイベントを処理したかどうか
+    ---@type boolean
+    IsRenderProcessed = false,
+
+    ---レンダーイベント内で実行する関数
+    render = function (self)
+        if not self.IsRenderProcessed then
+            for index, particle in ipairs(self.Particles) do
+                if particle:render() then
+                    table.remove(self.Particles, index)
+                    if #self.Particles == 0 then
+                        events.RENDER:remove("ex_skill_frame_particle_render")
+                        events.WORLD_RENDER:remove("ex_skill_frame_particle_world_render")
+                        self.IdCounter = 1
+                        self.IsRenderProcessed = false
+                    end
+                end
+            end
+            self.IsRenderProcessed = true
+        end
+    end,
+
+    ---ワールドレンダーイベント内で実行する関数
+    worldRender = function (self)
+        self.IsRenderProcessed = false
+    end,
+
+    ---パーティクルをスポーンさせる。
+    ---@param screenPos Vector2 パーティクルをスポーンさせるスクリーン上の座標。GUIスケールも考慮される。
+    spawn = function (self, screenPos)
+        local instance = FrameParticle.spawn(self.IdCounter, screenPos, math.random() > 0.98 and "FIGURA" or "NORMAL")
+        table.insert(self.Particles, instance)
+        if #self.Particles == 1 then
+            events.RENDER:register(function ()
+                self:render()
+            end, "ex_skill_frame_particle_render")
+            events.WORLD_RENDER:register(function ()
+                self:worldRender()
+            end, "ex_skill_frame_particle_world_render")
+        end
+    end,
+
+    ---初期化関数
+    init = function ()
+        ---@diagnostic disable-next-line: discard-returns
+        models:newPart("script_ex_skill_frame_particles", "Gui")
+    end
+}
+
+FrameParticleManager.init()
+
+return FrameParticleManager
