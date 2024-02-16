a915988b704024cf0a42712a559dac864f0acd19
Gakuto1112 <gakuto131112@gmail.com>
2024-02-16 20:52:26 +0900

    吹き出しエモートに表情を設定

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -992,6 +992,15 @@ BlueArchiveCharacter = {
             ---@param duration integer 吹き出しを再生する時間。-1は時間無制限を示す。
             ---@param showInGui boolean 一人称用にGUIに吹き出しを表示するかどうか
             onPlay = function(type, duration, showInGui)
+                if duration > 0 then
+                    if type == "GOOD" then
+                        FaceParts:setEmotion("NORMAL", "NORMAL", "CLOSED2", duration, true)
+                    elseif type == "HEART" then
+                        FaceParts:setEmotion("CLOSED", "CLOSED", "CLOSED", duration, true)
+                    elseif type == "QUESTION" then
+                        FaceParts:setEmotion("NARROW_ANGRY", "NARROW_ANGRY", "ANGRY", duration, true)
+                    end
+                end
             end,
 
             ---吹き出しアニメーション終了時に実行されるコールバック関数（任意）
@@ -999,6 +1008,9 @@ BlueArchiveCharacter = {
             ---@param type Bubble.BubbleType 再生された吹き出しエモートの種類
             ---@param forcedStop boolean 吹き出しエモートが途中終了した場合は"true"、吹き出しエモートが最後まで再生されて終了した場合は"false"が代入される。
             onStop = function(type, forcedStop)
+                if not forcedStop then
+                    FaceParts:resetEmotion()
+                end
             end
         }
     },
