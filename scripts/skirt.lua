0257cb234d6ea56ed0e2e28a62278340d5b7f84b
Gakuto1112 <gakuto131112@gmail.com>
2024-03-06 13:56:39 +0900

    スニーク時のスカート制御の遅延を修正

--- scripts/skirt.lua
+++ scripts/skirt.lua
@@ -1,5 +1,5 @@
 ---@class Skirt スカートを制御するクラス
 
 events.TICK:register(function()
-    models.models.main.Avatar.UpperBody.Body.Skirt:setRot(player:getPose() == "CROUCHING" and 30 or 0)
+    models.models.main.Avatar.UpperBody.Body.Skirt:setRot(player:isCrouching() and 30 or 0)
 end)
