230e1b537aa859747cd6a994130e909d58741c7a
Gakuto1112 <gakuto131112@gmail.com>
2024-05-03 20:42:09 +0900

    銃を構えた時の腕の揺れを追加

--- scripts/arms.lua
+++ scripts/arms.lua
@@ -12,6 +12,14 @@ Arms = {
     ---@type boolean
     BowPosePrev = false,
 
+    ---弓の構え時の微妙な手の揺れを再現するカウンター
+    ---@type number
+    ArmSwingCounter = 0,
+
+    ---このレンダーフレームでレンダー処理を終わらせたかどうか
+    ---@type boolean
+    IsRenderProcessed = false,
+
     ---弓の構えを行う。実際に弓を構えていなくても有効。
     ---@param enabled boolean 弓の構えの開始/終了
     ---@param leftHanded boolean 左手で構えているかどうか
@@ -24,6 +32,7 @@ Arms = {
                 modelPart:setRot()
             end
             events.RENDER:remove("bow_pose_render")
+            events.WORLD_RENDER:remove("bow_pose_world_render")
         end
 
         self.BowPoseEnabled = enabled
@@ -37,15 +46,28 @@ Arms = {
                         end
                         if events.RENDER:getRegisteredCount("bow_pose_render") == 0 then
                             events.RENDER:register(function ()
-                                local headRot = vanilla_model.HEAD:getOriginRot()
-                                if self.BowPoseLeftHanded then
-                                    models.models.main.Avatar.UpperBody.Arms.RightArm:setRot(headRot.x + 90, math.map((headRot.y + 180) % 360 - 180, -50, 50, -21, 78))
-                                    models.models.main.Avatar.UpperBody.Arms.LeftArm:setRot(headRot.x + 90, headRot.y)
-                                else
-                                    models.models.main.Avatar.UpperBody.Arms.RightArm:setRot(headRot.x + 90, headRot.y)
-                                    models.models.main.Avatar.UpperBody.Arms.LeftArm:setRot(headRot.x + 90, math.map((headRot.y + 180) % 360 - 180, -50, 50, -78, 21))
+                                if not self.IsRenderProcessed then
+                                    self.ArmSwingCounter = self.ArmSwingCounter + 0.2 / client:getFPS()
+                                    if self.ArmSwingCounter > 1 then
+                                        self.ArmSwingCounter = self.ArmSwingCounter - 1
+                                    end
+                                    local headRot = vanilla_model.HEAD:getOriginRot()
+                                    local armSwingOffset = math.sin(self.ArmSwingCounter * math.pi * 2) * 2.5
+                                    if self.BowPoseLeftHanded then
+                                        models.models.main.Avatar.UpperBody.Arms.RightArm:setRot(headRot.x + armSwingOffset + 90, math.map((headRot.y + 180) % 360 - 180, -50, 50, -21, 78), 0)
+                                        models.models.main.Avatar.UpperBody.Arms.LeftArm:setRot(headRot.x + armSwingOffset * -1 + 90, headRot.y, 0)
+                                    else
+                                        models.models.main.Avatar.UpperBody.Arms.RightArm:setRot(headRot.x + armSwingOffset + 90, headRot.y, 0)
+                                        models.models.main.Avatar.UpperBody.Arms.LeftArm:setRot(headRot.x + armSwingOffset * -1 + 90, math.map((headRot.y + 180) % 360 - 180, -50, 50, -78, 21), 0)
+                                    end
+                                    self.IsRenderProcessed = true
                                 end
                             end, "bow_pose_render")
+                        end
+                        if events.WORLD_RENDER:getRegisteredCount("bow_pose_world_render") == 0 then
+                            events.WORLD_RENDER:register(function ()
+                                self.IsRenderProcessed = false
+                            end, "bow_pose_world_render")
                         end
                     elseif not shouldBowPose and self.BowPosePrev then
                         stopBowPose()
