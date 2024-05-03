9d1152e738da66776836878e7a8fb0557330ccaf
Gakuto1112 <gakuto131112@gmail.com>
2024-05-03 22:04:32 +0900

    武器モデルの位置調整

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -117,15 +117,14 @@ BlueArchiveCharacter = {
             type = "NORMAL",
 
             ---一人称視点での位置オフセット（省略可）
-            --[[
             first_person_pos = {
                 ---右手で構える場合（省略可）
                 ---@type Vector3
-                right = vectors.vec3(0.85, 0.25),
+                right = vectors.vec3(0, 2, -6),
 
                 ---左手で構える場合（省略可）
                 ---@type Vector3
-                left = vectors.vec3(1.5)
+                left = vectors.vec3(2.25, 2, -6)
             },
 
             --[[
@@ -142,17 +141,15 @@ BlueArchiveCharacter = {
             ]]
 
             ---三人称視点での位置オフセット（省略可）
-            --[[
             third_person_pos = {
                 ---右手で構える場合（省略可）
                 ---@type Vector3
-                right = vectors.vec3(),
+                right = vectors.vec3(-1, 0, -7),
 
                 ---左手で構える場合（省略可）
                 ---@type Vector3
-                left = vectors.vec3()
-            }
-            ]]
+                left = vectors.vec3(3, 0, -7)
+            },
 
             --[[
             ---三人称視点での向きオフセット（省略可）
@@ -167,16 +164,18 @@ BlueArchiveCharacter = {
             }
             ]]
 
+            --[[
             ---装填済みクロスボウの位置オフセット（省略可）
             charged_crossbow_pos = {
                 ---右手で構える場合（省略可）
                 ---@type Vector3
-                right = vectors.vec3(-8.95, 1, 0),
+                right = vectors.vec3(),
 
                 ---左手で構える場合（省略可）
                 ---@type Vector3
-                left = vectors.vec3(9.05, 1, 0)
+                left = vectors.vec3()
             }
+            ]]
 
             ---装填済みクロスボウの向きオフセット（省略可）
             --[[
@@ -213,11 +212,11 @@ BlueArchiveCharacter = {
             rot = {
                 ---右手で構える場合（省略可）
                 ---@type Vector3
-                right = vectors.vec3(0, 0, 20),
+                right = vectors.vec3(-20, 90, 0),
 
                 ---左手で構える場合（省略可）
                 ---@type Vector3
-                left = vectors.vec3(0, 180, -20)
+                left = vectors.vec3(-20, -90, 0)
             }
         },
 
