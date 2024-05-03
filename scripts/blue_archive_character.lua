ca565217857f9c11600673be941fa1c8b7839874
Gakuto1112 <gakuto131112@gmail.com>
2024-05-03 20:02:55 +0900

    クロスボウ構え時の武器の位置を修正

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -160,7 +160,7 @@ BlueArchiveCharacter = {
                 ---左手で構える場合（省略可）
                 ---@type Vector3
                 left = vectors.vec3(1.5, -0.5, -3)
-            },
+            }
 
             --[[
             ---三人称視点での向きオフセット（省略可）
@@ -175,16 +175,18 @@ BlueArchiveCharacter = {
             }
             ]]
 
+            --[[
             ---装填済みクロスボウの位置オフセット（省略可）
             charged_crossbow_pos = {
                 ---右手で構える場合（省略可）
                 ---@type Vector3
-                right = vectors.vec3(-8.95, 1, 0),
+                right = vectors.vec3(),
 
                 ---左手で構える場合（省略可）
                 ---@type Vector3
-                left = vectors.vec3(9.05, 1, 0)
+                left = vectors.vec3()
             }
+            ]]
 
             ---装填済みクロスボウの向きオフセット（省略可）
             --[[
