1f53f4c38844952ad558ad7fea7fc697f432e98a
Gakuto1112 <gakuto131112@gmail.com>
2024-05-03 13:26:49 +0900

    baseの変更に適合するように変更

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -689,9 +689,13 @@ BlueArchiveCharacter = {
                 exSkill = 1
             },
 
-            swimsuit = {
+            {
+                ---コスチュームの内部名
+                ---@type string
+                name = "swimsuit",
+
                 ---コスチュームの表示名
-                name = {
+                display_name = {
                     ---英語
                     ---@type string
                     en_us = "Swimsuit",
