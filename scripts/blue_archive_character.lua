f5bab47578447fc6352117f5696df7e76c217a76
Gakuto1112 <gakuto131112@gmail.com>
2024-05-04 19:06:35 +0900

    ポートレートモデルから除外するモデルパーツを列挙

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -719,7 +719,7 @@ BlueArchiveCharacter = {
 
         ---頭のモデルパーツでポートレートから除外したいモデルパーツを配列形式で列挙する。
         ---@type ModelPart>[]
-        excludeModels = {}
+        excludeModels = {models.models.main.Avatar.Head.Phone}
     },
 
     ---物理演算データ
