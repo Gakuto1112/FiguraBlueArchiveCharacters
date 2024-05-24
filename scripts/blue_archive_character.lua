25de89c4f90957925a8741751e687cf909c126b3
Gakuto1112 <gakuto131112@gmail.com>
2024-05-24 23:38:05 +0900

    初期化時にもコールバック関数を呼ぶように変更

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -254,7 +254,6 @@ BlueArchiveCharacter = {
         ---利き手が変更された時に呼び出される関数。利き手に応じた銃やアクセサリーの変更に利用できる。
         ---@param direction Gun.HandDirection 新たな利き手
         onMainHandChange = function (direction)
-            print(direction)
         end
         ]]
     },
